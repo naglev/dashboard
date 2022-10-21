32cd362ec233d1241bb4334d12084562e627f9ff
naglev <105231947+naglev@users.noreply.github.com>
2022-10-21 09:52:18 +0200

    Update config_ix.py

--- dashboard/general_literature/indexing/config_ix.py
+++ dashboard/general_literature/indexing/config_ix.py
@@ -1,9 +1,3 @@
-"""
-Ezeket lehet inkább az indexerbe:
-languages
-file types
-directories to be indexed list()
-"""
 
 INDEXDIR =   'indexdir'
 WORKDIR =       'workdir'
@@ -11,6 +5,3 @@ THUMBNAILDIR =  'thumbnails'
 TESSERACT =     r"C:\KBApps\Tesseract-OCR\tesseract.exe"
 POPPLER_PATH =  r"C:\KBApps\Poppler\poppler-22.04.0\Library\bin"
 MAX_WORKERS =   4
-DIRECTORIES = [
-    r'C:\Users\ext-nagyle\Documents\Programming\Search_engine\misc_english_pdfs\search_engine\corpus'
-]
